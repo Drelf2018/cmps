eb3a6cd06666b6da59d439569c8a19c5ab6aa09f
Drelf2018 <drelf2018@outlook.com>
2023-08-12 01:47:19 +0800

    feat: 实现新功能
    合并小文件

--- cmps.go
+++ cmps.go
@@ -4,6 +4,7 @@ import (
 	"cmp"
 	"errors"
 	"reflect"
+	"slices"
 )
 
 var (
@@ -12,72 +13,135 @@ var (
 	SameType  = errors.New("the type of y must same as x")
 )
 
-func check(v any) reflect.Type {
-	typ := reflect.TypeOf(v)
-	if typ.Kind() == reflect.Ptr {
-		typ = typ.Elem()
+type Packages map[string]map[string]Type
+
+func (p Packages) Set(pkg, name string, typ reflect.Type) Type {
+	if p.Contain(pkg, name) {
+		return p[pkg][name]
 	}
-	if typ.Kind() != reflect.Struct {
-		panic(NotStruct)
-	}
-	return typ
+	c := Type{Type: typ, pkg: pkg, name: name}
+	c.parse()
+	p[pkg][name] = c
+	return c
 }
 
-func Compare(x, y any, options ...Type) int {
-	switch x := x.(type) {
-	case bool:
-		if x == y.(bool) {
-			return 0
-		}
-		if x {
-			return +1
-		}
-		return -1
-	case int:
-		return cmp.Compare(x, y.(int))
-	case int8:
-		return cmp.Compare(x, y.(int8))
-	case int16:
-		return cmp.Compare(x, y.(int16))
-	case int32:
-		return cmp.Compare(x, y.(int32))
-	case int64:
-		return cmp.Compare(x, y.(int64))
-	case uint:
-		return cmp.Compare(x, y.(uint))
-	case uint8:
-		return cmp.Compare(x, y.(uint8))
-	case uint16:
-		return cmp.Compare(x, y.(uint16))
-	case uint32:
-		return cmp.Compare(x, y.(uint32))
-	case uint64:
-		return cmp.Compare(x, y.(uint64))
-	case uintptr:
-		return cmp.Compare(x, y.(uintptr))
-	case float32:
-		return cmp.Compare(x, y.(float32))
-	case float64:
-		return cmp.Compare(x, y.(float64))
-	case string:
-		return cmp.Compare(x, y.(string))
-	default:
-		var t Type
-		if len(options) == 0 {
-			t = packages.Get(x)
-			if !t.Equal(y) {
-				panic(SameType)
-			}
-		} else {
-			t = options[0]
-		}
-		xv, yv := NewValue(x), NewValue(y)
-		for _, f := range t.Fields {
-			result := Compare(xv.Any(f.Index), yv.Any(f.Index), t.Child(f.Name))
-			if result != 0 {
-				return result
-			}
-		}
+func (p Packages) Contain(pkg, name string) bool {
+	types := p[pkg]
+	if types == nil {
+		types = make(map[string]Type)
+		p[pkg] = types
+		return false
+	}
+	_, ok := types[name]
+	return ok
+}
+
+func (p Packages) Get(v any) Type {
+	typ := checkValue(v)
+	return p.Set(typ.PkgPath(), typ.Name(), typ)
+}
+
+func compare[T cmp.Ordered](x, y any, t T) int {
+	return cmp.Compare(x.(T), y.(T))
+}
+
+func cmpbool(x, y any) int {
+	a := x.(bool)
+	if a == y.(bool) {
 		return 0
 	}
+	if a {
+		return +1
+	}
+	return -1
+}
+
+func Compare[T any](x, y T) int {
+	var t T = x
+	switch t := any(t).(type) {
+	case bool:
+		return cmpbool(x, y)
+	case int:
+		return compare(x, y, t)
+	case int8:
+		return compare(x, y, t)
+	case int16:
+		return compare(x, y, t)
+	case int32:
+		return compare(x, y, t)
+	case int64:
+		return compare(x, y, t)
+	case uint:
+		return compare(x, y, t)
+	case uint8:
+		return compare(x, y, t)
+	case uint16:
+		return compare(x, y, t)
+	case uint32:
+		return compare(x, y, t)
+	case uint64:
+		return compare(x, y, t)
+	case uintptr:
+		return compare(x, y, t)
+	case float32:
+		return compare(x, y, t)
+	case float64:
+		return compare(x, y, t)
+	case string:
+		return compare(x, y, t)
+	default:
+		typ := packages.Get(x)
+		if !typ.Equal(y) {
+			panic(SameType)
+		}
+		return typ.Compare(x, y)
+	}
+}
+
+func Slice[S ~[]E, E any](x S) {
+	slices.SortFunc(x, Compare)
+}
+
+func Keys[M ~map[K]V, K comparable, V any](m M) []K {
+	r := make([]K, 0, len(m))
+	for k := range m {
+		r = append(r, k)
+	}
+	Slice(r)
+	return r
+}
+
+func KeysToValues[M ~map[K]V, K comparable, V any](m M) []V {
+	r := make([]V, 0, len(m))
+	for i, k := range Keys(m) {
+		r[i] = m[k]
+	}
+	return r
+}
+
+func Values[M ~map[K]V, K comparable, V any](m M) []V {
+	r := make([]V, 0, len(m))
+	for _, v := range m {
+		r = append(r, v)
+	}
+	Slice(r)
+	return r
+}
+
+type reversedMap[K comparable, V any] struct {
+	k K
+	v V `cmps:"514"`
+}
+
+func ValuesToKeys[M ~map[K]V, K comparable, V any](m M) []K {
+	rm := make([]reversedMap[K, V], 0, len(m))
+	for k, v := range m {
+		rm = append(rm, reversedMap[K, V]{k, v})
+	}
+	Slice(rm)
+	s := make([]K, 0, len(m))
+	for i, r := range rm {
+		s[i] = r.k
+	}
+	return s
 }
--- cmps_test.go
+++ cmps_test.go
@@ -18,11 +18,32 @@ type Student struct {
 	} `cmps:"1919;Math,Chinese,English"`
 }
 
+func test1(t *testing.T) {
+	fs := []cmps.Field{
+		{Cmps: 1.14},
+		{Cmps: 5.14},
+		{Cmps: 4},
+		{Cmps: 2},
+		{Cmps: 3},
+		{Cmps: 0},
+	}
+	fmt.Printf("fs: %v\n", fs)
+	cmps.Slice(fs)
+	fmt.Printf("fs: %v\n", fs)
+}
+
+func test2(t *testing.T) {
+	fmt.Printf("cmps.Compare(true, true): %v\n", cmps.Compare(true, true))
+	fmt.Printf("cmps.Compare(true, false): %v\n", cmps.Compare(true, false))
+	fmt.Printf("cmps.Compare(false, true): %v\n", cmps.Compare(false, true))
+	fmt.Printf("cmps.Compare(false, false): %v\n", cmps.Compare(false, false))
+}
+
 func TestMain(t *testing.T) {
 	s1 := Student{
 		Name: "张三2",
 		ID:   1,
-		Male: false,
+		Male: true,
 		Scores: struct {
 			Chinese float64
 			Math    float64
@@ -30,13 +51,13 @@ func TestMain(t *testing.T) {
 		}{
 			Chinese: 100,
 			Math:    40,
-			English: 81,
+			English: 79,
 		},
 	}
 	s2 := Student{
 		Name: "张三2",
 		ID:   1,
-		Male: false,
+		Male: true,
 		Scores: struct {
 			Chinese float64
 			Math    float64
